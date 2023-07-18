1de2b870c43f98e1d0b157d0aaf923c0075a2352
onemafe <onemafe@gmail.com>
2023-07-18 12:28:46 +0300

    Перезагрузил репозиторий Git

---
+++ PracticON_Platformer_Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float _moveSpeed = 3;
+    [SerializeField] private float _jumpForce = 10;
+    [SerializeField] private float _friction = 0.6f;
+    [SerializeField] private float _maxSpeed = 5;
+
+    [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private Animator _animator;
+
+    private float coyoteTime = 0.2f; //Чем выше это значение, тем дольше игрок может прыгать после отрыва от земли
+    [SerializeField] private float coyoteTimeCounter; //Счётчик времени кайота
+
+    private float jumpBufferTime = 0.2f; //Сколько действует буффер
+    [SerializeField] private float jumpBufferCounter; //Счётчик времени буффера
+
+    [SerializeField] private float jumpFramesTimer;
+
+    private float _xAxis;
+    private float _xEuler;
+    private bool _isGrounded = false;
+
+
+
+
+
+
+
+    //Проверка isGrounded
+    private void OnCollisionStay(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            float angle = Vector3.Angle(collision.contacts[i].normal, Vector3.up);
+            if(angle < 45f)
+            {
+                _isGrounded = true;
+            }
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        _isGrounded = false;
+    }
+
+
+    //Нужны пояснения
+    private void Update()
+    {
+        _xAxis = Input.GetAxisRaw("Horizontal"); //Возвращает 1 -1
+
+        jumpFramesTimer += Time.deltaTime; //считается таймер для багов
+
+
+        // даётся время для прыжка койота. мы уже не grounded, но прыгнуть сможем, пока время убавляется в deltaTime
+        if (_isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+
+        //Возмодность отпрыгнуть, когда нажали прыжок во время прыжка
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+
+        if (coyoteTimeCounter > 0 && jumpBufferCounter > 0 && jumpFramesTimer > 0.4f)
+        {
+            jumpFramesTimer = 0;
+            jumpBufferCounter = 0;
+            Jump();
+        }
+
+        //Если отпустили Space
+        if(Input.GetKeyUp(KeyCode.Space) && _rigidBody.velocity.x > 0f)
+        {
+            coyoteTimeCounter = 0f;
+        }
+
+
+        if (_xAxis > 0)
+            _xEuler = 90f;
+        if (_xAxis < 0)
+            _xEuler = 270f;
+        transform.localRotation = Quaternion.Euler(0, _xEuler, 0);
+
+
+        _animator.SetBool("isRunning", Input.GetAxis("Horizontal") != 0);
+    }
+
+
+
+    private void Jump()
+    {
+        _animator.SetTrigger("jump");
+        _rigidBody.AddForce(0, _jumpForce, 0, ForceMode.VelocityChange);
+        Debug.Log("Прыжок");
+    }
+
+
+    private void FixedUpdate()
+    {
+        float input = Input.GetAxis("Horizontal");
+        float speedMultiplierInAir = 1f;
+
+        if(_isGrounded)
+        {
+            _rigidBody.AddForce(input * _moveSpeed, 0, 0, ForceMode.VelocityChange);
+        }
+        else
+        {
+            speedMultiplierInAir = 0.2f;
+
+            if (_rigidBody.velocity.x > _maxSpeed && input > 0)
+                speedMultiplierInAir = 0;
+            if (_rigidBody.velocity.x < -_maxSpeed && input < 0)
+                speedMultiplierInAir = 0;
+            _rigidBody.AddForce(input * _moveSpeed * speedMultiplierInAir, 0, 0, ForceMode.VelocityChange);
+        }
+
+        //трение по оси x добавляем, чтобы не ускоряться бесконечно. В воздухе оно становится в 0.2 раза меньше
+        _rigidBody.AddForce(-_rigidBody.velocity.x * _friction * speedMultiplierInAir, 0, 0, ForceMode.VelocityChange);
+    }
+
+
+
+    
+}
