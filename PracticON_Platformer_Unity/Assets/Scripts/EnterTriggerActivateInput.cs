c73986656dc1d79f8face9460f4d796cf7dd31f3
onemafe <onemafe@gmail.com>
2023-07-21 10:36:47 +0300

    GirlMovement

---
+++ PracticON_Platformer_Unity/Assets/Scripts/EnterTriggerActivateInput.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnterTriggerActivateInput : MonoBehaviour
+{
+
+    [SerializeField] private PlayerMovement _movementComponent;
+    [SerializeField] private bool _bool;
+
+
+
+    //Collider нужно держать в комплекте с префабом
+    private void OnTriggerEnter(Collider other)
+    {
+        _movementComponent.enabled = _bool;
+    }
+
+}
+
+
